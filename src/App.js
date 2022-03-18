460e188df86e88476325d9b2d42df4d438a3013e
likithliki5454 <likithlik54@gmail.com>
2022-03-18 15:45:42 +0530

    cascade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+   
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import data from './data.json';
+import "./App.css"
 
 function App() {
+  const [state, selState] = useState();
+  const [institutes, setInstitutes] = useState();
+  const [instList, setinstList] = useState();
+  const [output, setOutput] = useState();
+
+  const handleCountryChange = (obj) => {
+    selState(obj);
+    setinstList(obj.institutes);
+  };
+  
+
+  const handleLanguageChange = (obj) => {
+    setInstitutes(obj);
+  };
+
+  useEffect(() => {
+    if (state && institutes) {
+      setOutput(`id:${institutes.id} Name: ${institutes.Name} grad=${institutes.grad}`);
+    }
+  }, [state, institutes]);
+
+  const buttonClick=()=>{
+    if(state && institutes!="")
+    alert(state && institutes ? output : '')
+    else{
+      alert('please select the State and institute')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: 300, marginBottom: 20 }}>
+        <h1>State :</h1>
+        <Select
+          placeholder="Select State"
+          value={state}
+          options={data}
+          onChange={handleCountryChange}
+          getOptionLabel={x => x.state}
+        />
+        <br />
+        <h1>Institutes :</h1>
+        <Select
+          placeholder="Select institute"
+          options={instList}
+          onChange={handleLanguageChange}
+          getOptionLabel={x => x.Name}
+        
+        />
+      </div>
+      <button onClick={buttonClick}>Submit</button>
     </div>
   );
 }
